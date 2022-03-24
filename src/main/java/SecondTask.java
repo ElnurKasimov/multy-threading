fe13377cb5bc79cbc4339d81538ac76c1fc1691b
ElnurKasimov <ekasimov2012@gmail.com>
2022-03-24 21:14:46 +0200

    has fixed uncorrect exit from thread

--- src/main/java/SecondTask.java
+++ src/main/java/SecondTask.java
@@ -1,98 +1,103 @@
 import java.util.Scanner;
-import java.util.concurrent.Semaphore;
 
 public class SecondTask {
-    private static int counter=1;
-    private static Semaphore semaphore;
+    private static int counter = 1;
+    private static final Object MONITOR = new Object();
 
-    public  static  void fizz () {
-        if (counter %3 == 0 && counter%5 != 0) {
+    public static void fizz(int limit) {
+        if (counter % 3 == 0 && counter % 5 != 0 && counter <= limit) {
             System.out.print("fizz, ");
             counter++;
         }
     }
 
-    public  static  void buzz () {
-        if (counter %5 == 0 && counter%3 != 0) {
+    public static void buzz(int limit) {
+        if (counter % 5 == 0 && counter % 3 != 0 && counter <= limit) {
             System.out.print("buzz, ");
             counter++;
         }
     }
 
-    public  static  void fizzbuzz () {
-        if (counter %5 == 0 && counter%3 == 0) {
+    public static void fizzbuzz(int limit) {
+        if (counter % 5 == 0 && counter % 3 == 0 && counter <= limit) {
             System.out.print("fizzbuzz, ");
             counter++;
         }
     }
 
-    public  static  void number () {
-        if (counter %5 != 0 && counter%3 != 0) {
-            System.out.print( counter + ", ");
+    public static void number(int limit) {
+        if (counter % 5 != 0 && counter % 3 != 0 && counter <= limit) {
+            System.out.print(counter + ", ");
             counter++;
         }
     }
 
 
     public static void main(String[] args) {
-        semaphore = new Semaphore(4);
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Введите число, до которого должна работать программа : ");
-        int limit = sc.nextInt();
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Введите число, до которого должна работать программа : ");
+       int limit = sc.nextInt();
 
-        Thread threadA = new Thread( () ->{
-            while (counter <= limit ) {
-                try {
-                    semaphore.acquire();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            Thread threadA = new Thread(() -> {
+                while (counter < limit) {
+                    synchronized (MONITOR) {
+                        try {
+                            MONITOR.wait(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        fizz(limit);
+                    }
                 }
-                fizz();
-                semaphore.release();
-            }
-        });
+            });
 
-        Thread threadB = new Thread( () ->{
-            while (counter <= limit ) {
-                try {
-                    semaphore.acquire();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            Thread threadB = new Thread(() -> {
+                while (counter < limit) {
+
+                    synchronized (MONITOR) {
+                        try {
+                            MONITOR.wait(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        buzz(limit);
+                    }
                 }
-                buzz();
-                semaphore.release();
-            }
-        });
+            });
 
-        Thread threadC = new Thread( () ->{
-            while (counter <= limit ) {
-                try {
-                    semaphore.acquire();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            Thread threadC = new Thread(() -> {
+                while (counter < limit) {
+                    synchronized (MONITOR) {
+                        try {
+                            MONITOR.wait(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        fizzbuzz(limit);
+                    }
                 }
-                fizzbuzz();
-                semaphore.release();
-            }
-        });
+            });
 
-        Thread threadD = new Thread( () ->{
-            while (counter <= limit ) {
-                try {
-                    semaphore.acquire();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            Thread threadD = new Thread(() -> {
+                while (counter <= limit) {
+                    synchronized (MONITOR) {
+                        number(limit);
+                        MONITOR.notifyAll();
+                    }
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
                 }
-                number();
-                semaphore.release();
-            }
-        });
+            });
 
-        threadA.start();
-        threadB.start();
-        threadC.start();
-        threadD.start();
+            threadA.start();
+            threadB.start();
+            threadC.start();
+            threadD.start();
 
-    }
+        }
 }
